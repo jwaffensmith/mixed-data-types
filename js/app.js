ff2b0422d6c9b9d4ec24a0a1747b2339adf3884b
Username <jwaffensmith@gmail.com>
2021-07-10 17:58:05 -0700

    data types

---
+++ js/app.js
@@ -0,0 +1,44 @@
+/* === Question 1 Data Types === 
+
+- I would create properties in an object with booleans as values to represent the light switch being on or off. 
+(on and off as keys and true or false as values. 
+Example:  
+const lightSwitch = {
+    lightOn: true,
+    lightOff: false,
+};
+
+- I would use an object with the key being the username and the value being the email address as a string.
+Example: 
+const userEmail = {
+    username: "email@address.com"
+};
+
+- I would use an array with strings.
+Example:
+const spaceship = ["hull", "laser blasters" , "tractor beam", "warp drive"];
+
+- I would use an array with strings.
+Example: 
+const class = ["Jenna", "Student B", "Student C"];
+
+- I would use an object with names as the keys and locations as strings for the values.
+Example:
+const studentLocations = {
+    studentNameA: "Seattle",
+    studentNameB: "Los Angeles",
+    studentNameC: "Oakland",
+};
+
+- I would use an object that contains objects for each student. Names would be each object inside the object. 
+First property would be location with a string as a value. 
+The second property would have the key, "favoriteTvShows," with an array holding strings of each show. 
+Example: 
+const classInfo = {
+    studentName: {
+        Location:
+        favoriteTvShows: ["favorite tv show", "another one here", "another one here"];
+    }
+};
+
+*/
