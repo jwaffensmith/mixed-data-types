ee9de29b05b505da72b4a987ea2f462694d4c045
Username <jwaffensmith@gmail.com>
2021-07-10 18:44:52 -0700

    object-ception

--- js/app.js
+++ js/app.js
@@ -106,3 +106,26 @@ console.log(oneLarryNickname);
 
 let funkhauserObject = crazyObject.larry.characters[1];
 console.log(funkhauserObject);
+
+/* === Question 4 Object-ception === */
+
+const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
+    }
+ };
+
+ inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+
